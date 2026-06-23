aec4bc62f6abb0b22d2b93bef01f934ea380554c
Ricky Donald <rickydonaldr@gmail.com>
2026-06-23 21:23:08 +0530

    Remove @devantic/diaper dependency, delete favicon.svg, and update layout to use a new icon; modify manifest.json description and theme color.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import "./layout.css";
-	import favicon from "$lib/assets/favicon.svg";
+	import favicon from "$lib/assets/logos/icon.png";
     import { Toaster } from "svelte-sonner";
 
 	let { children } = $props();
